a042d6b9a0df724c60b0d42ba73bb3955e5ec33b
Jrius Kaxibwe <30425075+Jrius4@users.noreply.github.com>
2024-10-03 20:40:27 +0300

    navigation components

--- src/app/(dashboard)/layout.tsx
+++ src/app/(dashboard)/layout.tsx
@@ -1,6 +1,10 @@
 import type { Metadata } from "next";
 import { Inter } from "next/font/google";
 import "./../globals.css";
+import Link from "next/link";
+import Image from "next/image";
+import Menu from "@/components/Menu";
+import Navbar from "@/components/Navbar";
 
 const inter = Inter({ subsets: ['latin'] })
 
@@ -19,10 +23,19 @@ export default function DashboardLayout({
         <div
             className={`${inter.className} antialiased h-screen flex`}
         >
-            {/* LEFT */}
-            <div className="w-[14%] md:w-[8%] lg:w-[16%] xl:w-[14%] bg-red-400"></div>
+
+            <div className="w-[14%] md:w-[8%] lg:w-[16%] xl:w-[14%] p-4">
+                <Link href="/" className="flex items-center justify-center lg:justify-start gap-2">
+                    <Image src="/logo.png" alt="logo" width={32} height={32} />
+                    <span className="hidden lg:block">Kazibwe Schools</span>
+
+                </Link>
+                <Menu />
+            </div>
             {/* RIGHT */}
-            <div className="w-[86%] md:w-[92%] lg:w-[84%] xl:w-[86%] bg-yellow-300">{children}</div>
+            <div className="w-[86%] md:w-[92%] lg:w-[84%] xl:w-[86%]">
+                <Navbar />
+                {children}</div>
         </div>
 
     );
